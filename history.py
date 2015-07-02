727de4ffcc8220a6671781675f8563c9e0161904
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2015-07-01 23:23:32 -0400

    Change back to using just history.py
    Otherwise the pickled histories cannot be read correctly by
    the script in stacks-tools. Pickle is fickle!

--- history.py
+++ history.py
@@ -1527,3 +1527,19 @@ def do_it_all():
 	print "Here are some stats for the current history"
 	print
 	print_history_stats(History)
+
+# Uncomment the next two lines if you want to start from scratch
+#do_it_all()
+#exit(0)
+
+commit = raw_input("Which commit do you want to start with?\n")
+
+if not os.path.isfile("histories/" + commit):
+	print "ERROR: This commit does not exist in histories/"
+else:
+	print
+	print "Running this script will remove " + commit
+	yesno = raw_input("Are you sure you want to continue(yes/no)?\n")
+	if not yesno == "yes":
+		exit(0)
+	do_it_starting_with(commit)
